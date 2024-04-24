d86377ebc48e8db3795697c415f546da7942f82b
kashif8924 <kashifmahmood@pixarsart.com>
2024-04-24 19:51:55 +0500

    Changes Regarding validator and hash

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,7 +3,8 @@
 namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     //
@@ -19,21 +20,26 @@ class UserController extends Controller
         {
             return redirect()->back()->with('error', 'Email already exists in database');
         }
-        $specialChars = preg_match('/[^\w\s]/', $request->password);
-        $uppercaseLetter = preg_match('/[A-Z]/', $request->password);
+        $validator = Validator::make($request->all(), [
+            'password' => [
+                'required',
+                'string',
+                'min:8',
+                'regex:/^(?=.*[A-Z])(?=.*[^A-Za-z0-9])/',
+            ],
+        ]);
 
-        if (!$specialChars || !$uppercaseLetter) {
+        if ($validator->fails()) {
 
-            return redirect()->back( )->with('error','Not a valid password');
+            return redirect()->back()->with('error','Not a valid password');
         }
-
         $user = new User();
 
         $user->first_name = $request->first_name;
         $user->last_name = $request->last_name;
         $user->father_name = $request->father_name;
         $user->email = $request->email;
-        $user->password = $request->password;
+        $user->password = Hash::make($request->password);
         $user->save();
     }
 }
